d3c8c3469b69984aec680a13913efbca62181bde
Adeogo Adeniji <68272077+Adeogo1@users.noreply.github.com>
2023-07-22 21:55:17 +0100

    yhr

---
+++ PhysicsEngineOpengl/PhysicsEngine/Physics.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "RepeatedIncludes.h"
+#include "Maths.h"
+using namespace Maths;
+
+namespace Physics {
+	typedef struct _PointMass {
+		float mass;
+		Vector2 designatedPosition;
+		Vector2 correctedPosition;
+
+	} PointMass;
+
+
+
+
+
+};
